2b8b1509551b7d2f7a3a129e80822e60bedbeca5
John Alexis Guerra Gómez <john.guerra@gmail.com>
2025-11-18 14:09:44 -0800

    Supporting react router from express

--- backend.js
+++ backend.js
@@ -27,4 +27,9 @@ app.use(express.static(path.join(__dirname, "frontend/dist")));
 app.use("/api", indexRouter);
 app.use("/auth", authRouter);
 
+// Support react router
+app.get("*splat", (req, res) => {
+  res.sendFile(path.join(__dirname, "frontend/dist", "index.html"));
+});
+
 export default app;
