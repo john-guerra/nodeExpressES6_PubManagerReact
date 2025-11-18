10c1ca38c5d8909660cab1bb131c0c7b1649cbd3
John Alexis Guerra Gómez <john.guerra@gmail.com>
2025-11-18 12:16:04 -0800

    Adding a more elaborated auth

--- authConfig.js
+++ authConfig.js
@@ -4,21 +4,42 @@ import session from "express-session";
 
 // import crypto from "crypto";
 
+const users = [
+  {
+    id: 1,
+    username: "john",
+    name: "John Guerra",
+    // In a real application, never store passwords in plain text!
+    // Use proper hashing and salting mechanisms.
+    password: "secret",
+  },
+  {
+    id: 2,
+    username: "alexis",
+    name: "Alexis Sanchez",
+    // In a real application, never store passwords in plain text!
+    // Use proper hashing and salting mechanisms.
+    password: "sanchez",
+  },
+];
+
 export default function configurePassport(app) {
   const myStrategy = new LocalStrategy(function verify(username, password, cb) {
     console.log("🔑 verifying user", username);
 
     // TODO do proper user authentication
-    if (username === "john") {
+    const foundUser = users.find((u) => u.username === username);
+    if (foundUser) {
       console.log("🔐 user found:", username);
-      if (password !== "secret") {
+      if (password !== foundUser.password) {
         console.log("❌ incorrect password for user:", username);
         // Option 2. Valid user but wrong password
         return cb(null, false, { message: "Incorrect username or password." });
       }
+
+      delete foundUser.password; // Remove password before storing in session
       // Option 3. Valid user and correct password
-      const user = { id: 1, username: "john", name: "John Guerra" };
-      return cb(null, user);
+      return cb(null, foundUser);
     } else {
       // Option 1. Invalid user or error
       const err = "Incorrect username or password.";
